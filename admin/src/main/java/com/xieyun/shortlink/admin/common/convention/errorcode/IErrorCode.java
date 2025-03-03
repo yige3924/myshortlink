609508face23c5c5b851a9402e2f144ca4dce94e
yige3924 <yexieyun@gmail.com>
2025-03-03 22:28:59 +0800

    feature: 开发短链接规约功能

---
+++ admin/src/main/java/com/xieyun/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,19 @@
+package com.xieyun.shortlink.admin.common.convention.errorcode;
+
+/**
+ * @author XieYun
+ * @version 1.0
+ * 平台错误码
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
