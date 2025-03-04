a0e352f9c2cdfa7a6c62dd430e9e2426da35d9a0
yige3924 <yexieyun@gmail.com>
2025-03-04 14:48:05 +0800

    feature: 开发短链接后台管理系统通用持久层实体对象属性填充

---
+++ admin/src/main/java/com/xieyun/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.xieyun.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author XieYun
+ * @version 1.0
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
