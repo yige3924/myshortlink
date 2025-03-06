29d8d0dc7af13e3ee13d940fdd584b3e10711f0c
yige3924 <yexieyun@gmail.com>
2025-03-06 16:00:49 +0800

    feature: 短链接后管系统调用中台请求相关接口

---
+++ admin/src/main/java/com/xieyun/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,18 @@
+package com.xieyun.shortlink.admin.remote.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+
+/**
+ * @author XieYun
+ * @version 1.0
+ * 短链接分页请求参数
+ */
+@Data
+public class ShortLinkPageReqDTO extends Page {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
---
+++ admin/src/main/java/com/xieyun/shortlink/admin/remote/dto/resp/ShortLinkPageRespDTO.java
@@ -0,0 +1,74 @@
+package com.xieyun.shortlink.admin.remote.dto.resp;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author XieYun
+ * @version 1.0
+ * 短链接分页响应参数
+ */
+@Data
+public class ShortLinkPageRespDTO {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUri;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 有效期类型 0: 永久有效 1: 自定义
+     */
+    private Integer validDateType;
+
+    /**
+     * 有效期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 描述
+     * 这是MySQL中的关键字，需要@TableField("`describe`")，反引号
+     */
+    @TableField("`describe`")
+    private String describe;
+
+    /**
+     * 网站标识
+     */
+    private String favicon;
+}
