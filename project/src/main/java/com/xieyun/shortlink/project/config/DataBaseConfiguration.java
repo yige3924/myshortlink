f7122d76460313915be0a1035296a4a4f2e8161d
yige3924 <yexieyun@gmail.com>
2025-03-06 15:14:30 +0800

    feature: 提交MySQL数据库分页插件

---
+++ project/src/main/java/com/xieyun/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,24 @@
+package com.xieyun.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author XieYun
+ * @version 1.0
+ */
+
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
