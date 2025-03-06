3cff75fc1cc42ffcdbf7a54c443937aec1d6c08f
yige3924 <yexieyun@gmail.com>
2025-03-06 15:30:00 +0800

    feature: 开发短链接分页查询功能

--- project/src/main/java/com/xieyun/shortlink/project/config/DataBaseConfiguration.java
+++ project/src/main/java/com/xieyun/shortlink/project/config/DataBaseConfiguration.java
@@ -3,6 +3,7 @@ package com.xieyun.shortlink.project.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -16,6 +17,7 @@ public class DataBaseConfiguration {
     /**
      * 分页插件
      */
+    @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
