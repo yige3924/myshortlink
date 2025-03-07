514f79b3a43aaf3edcf68d09a655812d704f213e
yige3924 <yexieyun@gmail.com>
2025-03-07 17:36:50 +0800

    feature: 短链接服务开发修改功能

--- project/src/main/java/com/xieyun/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/xieyun/shortlink/project/service/ShortLinkService.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.xieyun.shortlink.project.dao.entity.ShortLinkDO;
 import com.xieyun.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.xieyun.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.xieyun.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
 import com.xieyun.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.xieyun.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.xieyun.shortlink.project.dto.resp.ShortLinkPageRespDTO;
@@ -20,9 +21,15 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
     /**
      * 创建短链接
      * @param requestParam 创建短链接请求参数
-     * @return 短链接创建参数
+     * @return 短链接创建信息
      */
     ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+
+    /**
+     * 修改短链接
+     * @param requestParam 修改短链接请求参数
+     */
+    void updateShortLink(ShortLinkUpdateReqDTO requestParam);
 
     /**
      * 分页查询短链接
@@ -37,4 +44,5 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
      * @return 查询短链接分组内数量响应
      */
     List<ShortLinkGroupCountQueryRespDTO> listGroupShortLinkCount(List<String> requestParam);
+
 }
