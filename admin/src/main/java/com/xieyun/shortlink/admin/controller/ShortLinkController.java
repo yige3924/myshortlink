5aa3bafae3a472367e6e55633355959aa8c57669
yige3924 <yexieyun@gmail.com>
2025-03-07 17:56:39 +0800

    feature: 短链接后管开发短链接修改功能

--- admin/src/main/java/com/xieyun/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/xieyun/shortlink/admin/controller/ShortLinkController.java
@@ -2,15 +2,14 @@ package com.xieyun.shortlink.admin.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xieyun.shortlink.admin.common.convention.result.Result;
+import com.xieyun.shortlink.admin.common.convention.result.Results;
 import com.xieyun.shortlink.admin.remote.ShortLinkRemoteService;
 import com.xieyun.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.xieyun.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.xieyun.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.xieyun.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.xieyun.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author XieYun
@@ -30,6 +29,15 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/admin/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 修改短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        ShortLinkRemoteService.updateShortLink(requestParam);
+        return Results.success();
     }
 
     /**
--- admin/src/main/java/com/xieyun/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/xieyun/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xieyun.shortlink.admin.common.convention.result.Result;
 import com.xieyun.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.xieyun.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.xieyun.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.xieyun.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.xieyun.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.xieyun.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
@@ -21,6 +22,16 @@ import java.util.Map;
  * 短链接中台远程调用服务
  */
 public interface ShortLinkRemoteService {
+
+    /**
+     * 修改短链接
+     *
+     * @param requestParam 修改短链接请求参数
+     */
+    static void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
+        HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/update",
+                JSON.toJSONString(requestParam));
+    }
 
     /**
      * 创建短链接
