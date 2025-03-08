fdfd50abb712371f3189cf381fcee80f0c004ece
yige3924 <yexieyun@gmail.com>
2025-03-08 15:24:09 +0800

    feature: 开发短链接监控之基础访问PV数据

---
+++ project/src/main/java/com/xieyun/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -0,0 +1,20 @@
+package com.xieyun.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xieyun.shortlink.project.dao.entity.LinkAccessStatsDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author XieYun
+ * @version 1.0
+ * 短链接基础访问监控持久层
+ */
+public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
+
+    @Insert("INSERT INTO " +
+            "t_link_access_stats (full_short_url, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag) " +
+            "VALUES( #{linkAccessStats.fullShortUrl}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE pv = pv +  #{linkAccessStats.pv}, uv = uv + #{linkAccessStats.uv}, uip = uip + #{linkAccessStats.uip};")
+    void shortLinkStats(@Param("linkAccessStats") LinkAccessStatsDO linkAccessStatsDO);
+}
